e0f11e5ddc4ff99c42000068624080f47756db7b
jaime <jaimepereirasantos123@gmail.com>
2021-11-15 20:23:34 +0000

    Exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    let newStr = "";
+    let i = str.length-1;
+    for(let j = 0; j<str.length; j++,i--){
+        newStr += str.charAt(i);
+    }
+    return newStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...element) {
+    let helperArray = [];
+    for(let i=0; i<array.length;i++){
+        if(array[i]!=element){
+            helperArray[i] = array[i];
+        }
+    }
+    array = helperArray;
+    return array
 
 };
 
