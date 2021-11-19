dae585166e6572d1f5a0c528b1cc3c7363cdc62e
jaime <jaimepereirasantos123@gmail.com>
2021-11-19 14:02:16 +0000

    Foundation: Fundamentals5 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = (n)=> {
+    n=Number(n)
+    return (n<0 ? "OOPS" : 
+    (n<=1 ? n : 
+        fibonacci(n-2) + fibonacci(n-1)))
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+console.log(fibonacci(5))
