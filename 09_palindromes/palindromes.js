decf26ef5c489ea7c3b6818699f8bd37b4e4b410
jaime <jaimepereirasantos123@gmail.com>
2021-11-19 13:42:36 +0000

    Foundation: Fundamentals5 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes =(str)=>{
+    let re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    for(let i = 0; i<str.length-1; i++){
+        if(str[i] !== str[str.length-i-1]){
+            console.log(`1 : ${str[i]}\t2 : ${str[str.length - i -1]}`)
+            return false;
+        }
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
