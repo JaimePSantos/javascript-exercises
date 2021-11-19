f9d575113b183229323fb15c9651586bbd56e699
jaime <jaimepereirasantos123@gmail.com>
2021-11-19 13:17:07 +0000

    Foundation: Fundamentals5 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,22 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
-};
+const reducerSum = (previousValue,currentValue) => previousValue + currentValue;
 
-const multiply = function() {
+const sum = (arr) => arr.reduce(reducerSum,0);
 
-};
+const reducerMult = (previousValue,currentValue)=>previousValue * currentValue;
 
-const power = function() {
-	
-};
+const multiply = (arr) => arr.reduce(reducerMult);
 
-const factorial = function() {
-	
-};
+const power = (base,pwr) => base**pwr;
+
+const factorial = (n) =>(n==0||n==1)? 1 : (n * factorial(n-1))
 
 // Do not edit below this line
 module.exports = {
